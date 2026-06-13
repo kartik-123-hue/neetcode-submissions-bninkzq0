1b01f1f6db2bcc204d2ffced47dd60d7dd6803ac
Kartik Mehta <72431395+kartik-123-hue@users.noreply.github.com>
2026-06-13 18:04:02 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count=new int[26];
+        for(char c:tasks){
+            count[c-'A']++;
+        }
+
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        for(int cnt:count){
+            if(cnt>0){
+                maxHeap.offer(cnt);
+            }
+        }
+        Queue<int[]> q=new LinkedList<>();
+        int currentTime=0;
+        while(!maxHeap.isEmpty() || !q.isEmpty()){
+            currentTime++;
+            if(maxHeap.isEmpty()){
+                
+                currentTime=q.peek()[1];
+            }
+            else{
+                int remainingCount=maxHeap.poll()-1;
+                if(remainingCount>0){
+                    System.out.println("what is the time when it was changed "+remainingCount+" the time "+(currentTime+n));
+                    q.offer(new int[]{remainingCount,currentTime+n});
+                }
+            }
+            if(!q.isEmpty() && q.peek()[1]==currentTime){
+                maxHeap.offer(q.poll()[0]);
+            }
+        }
+        return currentTime;
+    }
+}
