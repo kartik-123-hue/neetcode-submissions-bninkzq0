4a80e938102ca53d2dbcb43a578c1a4106111b6b
Kartik Mehta <72431395+kartik-123-hue@users.noreply.github.com>
2026-06-18 21:05:58 +0530

    Add: redistribute-characters-to-make-all-strings-equal - submission-3

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int[] freq=new int[26];
+        int flag=0;
+        int n=words.length;
+        for(String word:words){
+            for(char c : word.toCharArray()){
+                int i=c-'a';
+                if(freq[i]!=0){
+                    freq[i]++;
+                    if(freq[i]%n==0){
+                        flag++;
+                    }
+                }
+                else{
+                    freq[i]++;
+                    if(freq[i]%n!=0){
+                        flag--;
+                    }
+                }
+                //this means we are modding to check if the char are arranged and used in others if yes it will have 0 as the remainder
+                freq[i]%=n;
+            }
+        }
+        return flag==0;
+    }
+}
