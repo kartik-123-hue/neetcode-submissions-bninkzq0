2251155b86f530fe891423163507504b5aafc2cd
Kartik Mehta <72431395+kartik-123-hue@users.noreply.github.com>
2026-07-06 01:14:01 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.java
@@ -0,0 +1,44 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node,Node> table=new HashMap<>();
+    public Node cloneGraph(Node node) {
+        Node temp=dfs(node);
+        return table.get(node);
+    }
+    public Node dfs(Node head){
+        if(head==null){
+            return null;
+        }
+        Node newNode=new Node(head.val);
+        table.put(head,newNode);
+        for(Node nei:head.neighbors){
+            if(table.containsKey(nei)){
+                newNode.neighbors.add(table.get(nei));
+            }
+            else{
+                newNode.neighbors.add(dfs(nei));
+            }
+        }
+        return newNode;
+    }
+
+}
